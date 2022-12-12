1aff248ac012c8e4f95556ca57beca08ea877058
Bailey Mejia <baileymejia28@gmail.com>
2022-12-11 16:11:48 -0800

    Added API routes for CRUD operations and seeded the database to test my routes.

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -3,28 +3,3 @@ DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
-
---CREATE TABLE
-CREATE TABLE Category (
-    id INT NOT NULL PRIMARY KEY(id) AUTO INCREMENT,
-    category_name VARCHAR(100) NOT NULL
-);
---CREATE TABLE product
-CREATE TABLE product (
-    id INT NOT NULL PRIMARY KEY(id) AUTO INCREMENT,
-    product_name VARCHAR(100) NOT NULL,
-    price DECIMAL NOT NULL,
-    stock INT NOT NULL SET DEFAULT VALUE(10),
-    category_id INT REFERENCE(id)
-);
---CREATE TABLE tag
-CREATE TABLE tag (
-    id INT NOT NULL PRIMARY KEY(id) AUTO INCREMENT,
-    tag_name VARCHAR(100)
-);
---CREATE TABLE ProductTag
-CREATE TABLE ProductTag (
-    id INT NOT NULL PRIMARY KEY(id) AUTO INCREMENT,
-    product_id INT REFERENCE(id),
-    tag_id INT REFERENCE(id)
-);
--- Develop/server.js
+++ Develop/server.js
@@ -14,5 +14,24 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 Sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log(`${PORT}`));
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 });
+
+//TODO sync sequelize to the server.js page **DONE
+
+//TODO add password info to run the server to see if it works
+
+//TODO add data to tables in schema page **DONE
+
+//TODO Add code to the models to add values to the databases **DONE
+
+//TODO look at the routes folder and add code if needed 
+//TODO add a delete function to delete the data from the table
+
+//TODO look and code to the seeds folder **DONE
+
+//TODO finish product routes file 
+
+//TODO finish tag routes file
+
+//TODO find out why I am getting unknown database
